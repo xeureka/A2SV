4464b8dfbf1a49fb7c5253f02eda786bca27bfcb
xeureka <156827630+xeureka@users.noreply.github.com>
2025-01-28 16:27:07 +0300

    Created 'Palindrome Number' by A2SV Hub

---
+++ 28-Jan-2025/Palindrome Number 225757.py
@@ -0,0 +1,18 @@
+# Problem: Palindrome Number - https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        if x < 0:
+            return False
+        
+        
+        bucket = x
+        rev_num = 0
+
+        while x != 0:
+            digit = x % 10
+            rev_num = rev_num * 10 + digit
+            x //= 10
+        
+        return bucket == rev_num
