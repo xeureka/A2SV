06b677c940ef103578d135e1a3d05fb85e3299f9
xeureka <156827630+xeureka@users.noreply.github.com>
2025-01-29 14:11:16 +0300

    Created 'Thearte Square' by A2SV Hub

---
+++ 29-Jan-2025/Thearte Square 225914.py
@@ -0,0 +1,10 @@
+# Problem: Thearte Square - https://codeforces.com/problemset/problem/1/A
+
+from math import ceil
+
+n,m,a = map(int,input().split())
+
+right = ceil(n/a)
+left = ceil(m/a)
+
+print(int(right*left))
