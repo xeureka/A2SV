b80cc9a3fca96f83a6df0d1707d8e6aeca8098cf
xeureka <156827630+xeureka@users.noreply.github.com>
2025-01-29 14:27:39 +0300

    Created 'Weird' by A2SV Hub

---
+++ 29-Jan-2025/Weird 225921.py
@@ -0,0 +1,26 @@
+# Problem: Weird - https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+    if n % 2 != 0:
+        print('Weird')
+    
+    else:
+        if 2 <= n <= 5:
+            print('Not Weird')
+        elif 6 <= n <= 20:
+            print('Weird')
+        else:
+            print('Not Weird')
+    
