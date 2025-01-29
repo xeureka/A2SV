50c15653a6fa6000646abe39f38a9dff44761ce2
xeureka <156827630+xeureka@users.noreply.github.com>
2025-01-29 15:42:09 +0300

    Created 'Mod Power' by A2SV Hub

---
+++ 29-Jan-2025/Mod Power 225935.py
@@ -0,0 +1,7 @@
+# Problem: Mod Power - https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(n):
+        print(i**2)
