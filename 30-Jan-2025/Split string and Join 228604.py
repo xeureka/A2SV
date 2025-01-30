d65660f480b421c6dca11d4cc8a8d2a0aeb07e81
xeureka <156827630+xeureka@users.noreply.github.com>
2025-01-30 22:37:25 +0300

    Created 'Split string and Join' by A2SV Hub

---
+++ 30-Jan-2025/Split string and Join 228604.py
@@ -0,0 +1,10 @@
+# Problem: Split string and Join - https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
+
+def split_and_join(line):
+    # write your code here
+    return "-".join(line.split())
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
