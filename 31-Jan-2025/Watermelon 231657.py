f082076b6d6c5781ad179093340d35aaca210afa
xeureka <156827630+xeureka@users.noreply.github.com>
2025-01-31 19:26:40 +0300

    Created 'Watermelon' by A2SV Hub

---
+++ 31-Jan-2025/Watermelon 231657.py
@@ -0,0 +1,8 @@
+# Problem: Watermelon - https://codeforces.com/problemset/problem/4/A
+
+    w = int(input())
+     
+    if w %2 ==0 and w != 2:
+        print('YES')
+    else:
+        print('NO')
