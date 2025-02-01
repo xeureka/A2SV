8f8d14035917d747a084efdc2dc5fde700018cf5
xeureka <156827630+xeureka@users.noreply.github.com>
2025-02-01 22:40:51 +0300

    Created 'List Comprehension' by A2SV Hub

---
+++ 01-Feb-2025/List Comprehension 233913.py
@@ -0,0 +1,18 @@
+# Problem: List Comprehension - https://www.hackerrank.com/challenges/list-comprehensions/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    
+    lst = []
+
+    for i in range(x+1):
+        for j in range(y+1):
+            for k in range(z+1):
+                if i + j + k != n:
+                    lst.append([i,j,k])
+
+    print(lst)
