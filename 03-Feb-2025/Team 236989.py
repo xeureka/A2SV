b424422764c3e02232fda11e768ab0064cd4d709
xeureka <156827630+xeureka@users.noreply.github.com>
2025-02-03 20:19:09 +0300

    Created 'Team' by A2SV Hub

---
+++ 03-Feb-2025/Team 236989.py
@@ -0,0 +1,17 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+
+
+answer = 0
+
+
+for i in range(int(input())):
+    
+    lst = list(map(int,input().split()))
+    
+    if lst.count(1) >= 2:
+        answer += 1
+
+    
+print(answer)
+
