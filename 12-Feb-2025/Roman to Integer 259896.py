e61bcea1d7ed9ee190904fff8d1262de69aec607
xeureka <156827630+xeureka@users.noreply.github.com>
2025-02-12 15:39:54 +0300

    Created 'Roman to Integer' by A2SV Hub

---
+++ 12-Feb-2025/Roman to Integer 259896.py
@@ -0,0 +1,23 @@
+# Problem: Roman to Integer - https://leetcode.com/problems/roman-to-integer/?envType=problem-list-v2&envId=string
+
+    roman_lett = {'I': 1,
+     'V': 5,
+      'X': 10
+      , 'L': 50,
+       'C': 100,
+        'D': 500,
+         'M': 1000}
+    
+    ans = 0
+    
+    for i in range(0, len(s)):
+        
+        if i < len(s) - 1 and roman_lett[s[i]] < roman_lett[s[i+1]]:
+            
+            ans -= roman_lett[s[i]]
+            
+   
+        else:
+            ans += roman_lett[s[i]]
+            
+    return ans
