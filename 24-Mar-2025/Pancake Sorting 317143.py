6e139b69f7e31fa85de54aad74b76d13c5bdc8f0
xeureka <156827630+xeureka@users.noreply.github.com>
2025-03-24 20:45:10 +0300

    Created 'Pancake Sorting' by A2SV Hub

---
+++ 24-Mar-2025/Pancake Sorting 317143.py
@@ -0,0 +1,10 @@
+# Problem: Pancake Sorting - https://leetcode.com/problems/pancake-sorting/
+
+class Solution:
+    def pancakeSort(self, A):
+        res = []
+        for x in range(len(A), 1, -1):
+            i = A.index(x)
+            res.extend([i + 1, x])
+            A = A[:i:-1] + A[:i]
+        return res
